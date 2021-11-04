35f0d48279cda3b72ef7d33505d0544129786ae9
luca-bottero <lucabottero99@gmail.com>
2021-11-04 18:23:02 +0100

    add webcam graph

--- webcam.py
+++ webcam.py
@@ -6,14 +6,27 @@ cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
+def manipulate_frame(frame):
+    '''
+    This function acts on a cv frame
+    '''
+
+    pass
+
+
 while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    ret, frame = cap.read()     #ret is aflag that indicates if the frame has been captured
+    frame = cv2.resize(frame, None, fx=1., fy=1., interpolation=cv2.INTER_AREA)
     cv2.imshow('Input', frame)
+
+    manip_res = manipulate_frame(frame)
+
+    cv2.putText(frame, "OpenCV + Jurassic Park!!!", (10, 25), 
+	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
     c = cv2.waitKey(1)
     if c == 27:
-        break
+        break       #27 is ascii for ESC
 
 cap.release()
 cv2.destroyAllWindows()
